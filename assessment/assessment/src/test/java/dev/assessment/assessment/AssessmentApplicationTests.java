4f740697bb57dd87b5eae787a42ba338a160a326
BerkaySozer <83730857+BerkaySozer@users.noreply.github.com>
2023-07-26 18:04:15 +0300

    Add files via upload

---
+++ assessment/assessment/src/test/java/dev/assessment/assessment/AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.assessment.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
